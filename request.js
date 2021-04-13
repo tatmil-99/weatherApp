529c0d2f3b0d5e3fb4bb651ee277f3fb81c9e288
Tatien Miller <tatmil99@gmail.com>
2021-04-13 13:55:22 -0500

    initial fetch created and logs object

--- request.js
+++ request.js
@@ -1,3 +1,15 @@
 const key = "369d22c525938e841fa5991e68e8d7d0";
 
-const baseUrl = "api.openweathermap.org/data/2.5/weather?q=Kansas City&appid=369d22c525938e841fa5991e68e8d7d0";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=Kansas City&appid=369d22c525938e841fa5991e68e8d7d0";
+
+const fetchWeather = async () => {
+  try {
+    let response = await fetch(baseUrl);
+    let json = await response.json();
+    console.log(json);
+  } catch(err) {
+    alert(err);
+  }
+}
+
+fetchWeather();
