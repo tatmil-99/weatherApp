465b027484ba5dff1c90c847ddc995aa71f43042
Tatien Miller <tatmil99@gmail.com>
2021-04-13 17:48:17 -0500

    request works and takes user input and displays it in console

--- request.js
+++ request.js
@@ -1,15 +1,30 @@
 const key = "369d22c525938e841fa5991e68e8d7d0";
 
-const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=Kansas City&appid=369d22c525938e841fa5991e68e8d7d0";
+// const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=Kansas City&appid=369d22c525938e841fa5991e68e8d7d0";
 
-const fetchWeather = async () => {
+// const fetchWeather = async () => {
+//   try {
+//     let response = await fetch(baseUrl);
+//     let json = await response.json();
+//     console.log(json);
+//   } catch(err) {
+//     alert(err);
+//   }
+// }
+
+// fetchWeather();
+
+const requestCity = async (city) => {
   try {
-    let response = await fetch(baseUrl);
+    const url = "https://api.openweathermap.org/data/2.5/weather";
+    const query = `?q=${city}&appid=${key}`;
+
+    let response = await fetch(url + query);
     let json = await response.json();
-    console.log(json);
+
+    return json;
   } catch(err) {
     alert(err);
-  }
-}
+  };
+};
 
-fetchWeather();
---
+++ script.js
@@ -0,0 +1,8 @@
+const searchForm = document.querySelector(".search-location");
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const cityValue = document.querySelector(".search-location input").value;
+  console.log(cityValue);
+  searchForm.reset();
+});
